607f2581a98ceb617c7bfc95d58d5a825c4408cb
Matthias <48438784+speedswimmer@users.noreply.github.com>
2022-03-04 08:22:30 +0100

    Create dht22.py

---
+++ dht22.py
@@ -0,0 +1,18 @@
+import Adafruit_DHT
+import time
+
+sensor = Adafruit_DHT.DHT22
+pin = 12
+
+print("[press ctrl+c to stop the script]...")
+
+try:
+        while True:
+                humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
+                time.sleep(30)
+                if humidity is not None and temperature is not None:
+                        print("Temp={0:0.1f}*C Humidity={1:0.1f}%".format(temperature, humidity))
+                else:
+                        print("Failed to get reading. Try again!")
+except KeyboardInterrupt:
+        print("Script stopped!")
