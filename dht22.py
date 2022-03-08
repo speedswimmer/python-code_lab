a6c18f215510529e9b29ac0f7c04cc1fd6c5addb
Matthias <48438784+speedswimmer@users.noreply.github.com>
2022-03-08 15:35:12 +0100

    Update dht22.py

--- dht22.py
+++ dht22.py
@@ -1,6 +1,7 @@
 import Adafruit_DHT
 import time
 from datetime import datetime
+import json
 
 sensor = Adafruit_DHT.DHT22
 pin = 12
@@ -10,12 +11,20 @@ print("[press ctrl+c to stop the script]...")
 try:
         while True:
                 now = datetime.now()
+                env_data = {}
+                entry = {}
                 timestamp = now.strftime('%d.%m.%Y %H:%M')
                 humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
                 if humidity is not None and temperature is not None:
+                        env_data["Temp"]=round(temperature, 1)
+                        env_data["Hum"]=round(humidity, 1)
+                        entry[timestamp]=env_data
                         print(timestamp, "Temp={0:0.1f}°C, Humidity={1:0.1f}%".format(temperature, humidity))
+                        with open ("environment.json", "a") as f:
+                                json.dump(entry, f)
                 else:
                         print("Failed to get reading. Try again!")
-                time.sleep(60)
+                time.sleep(300)
+
 except KeyboardInterrupt:
         print("Script stopped!")
