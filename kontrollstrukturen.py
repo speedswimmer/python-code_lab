079736a52c77e9a063f16f7caea67f42b8d990fd
Matthias <48438784+speedswimmer@users.noreply.github.com>
2022-03-24 18:39:00 +0100

    Delete kontrollstrukturen.py

--- kontrollstrukturen.py
+++
@@ -1,9 +0,0 @@
-zahl = input("Gib eine Zahl ein: ")
-zahl = int(zahl)
-
-if zahl > 50:
-    print("Die Zahl ist größer 50!")
-elif zahl < 25:
-    print("Deine Zahl ist kleiner 25!")
-else:
-    print("Die Zahl ist kleiner 50!")
