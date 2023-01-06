cb3ce4fe092c50969bc174d960abb7f7808f51a6
Matthias <48438784+speedswimmer@users.noreply.github.com>
2023-01-06 09:45:04 +0100

    Update dht22.py

--- dht22.py
+++ dht22.py
@@ -18,14 +18,16 @@ try:
                 timestamp = now.strftime('%d.%m.%Y %H:%M')
                 humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
                 if humidity is not None and temperature is not None:
+                        entry = read_file()
                         env_data["Temp"]=round(temperature, 1)
                         env_data["Hum"]=round(humidity, 1)
                         entry[timestamp]=env_data
                         print(timestamp, "Temp={0:0.1f}°C, Humidity={1:0.1f}%".format(temperature, humidity))
-                        with open ("environment.json", "a") as f:
-                                json.dump(entry, f)
+                        with open ("/home/pi/Scripts/templog.txt", "a") as d:
+                                d.write(f'{timestamp}: Temperature = {temperature:3.1f}°C, Humidity {humidity:3.1f}%\n')
                 else:
                         print("Failed to get reading. Try again!")
+                        pass
                 time.sleep(interval)
 
 except KeyboardInterrupt:
